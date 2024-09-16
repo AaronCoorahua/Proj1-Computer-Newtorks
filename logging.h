272586b89ad741bbdf2da3dfd66a15b044e3f143
AaronCoorahua <83974253+AaronCoorahua@users.noreply.github.com>
2024-09-15 21:10:06 -0600

    Update logging.h

--- logging.h
+++ logging.h
@@ -2,46 +2,43 @@
 #define LOGGING_H
 
 #include <iostream>
-#include <filesystem>
 #include <string>
-#include <iomanip>
 
 inline int LOG_LEVEL = 3;
 
-// Logging functions with line and file parameters
-inline void logTrace(const std::string& message, const char* file, int line) {
+void logTrace(const std::string &msg, const char *file, int line) {
     if (LOG_LEVEL > 5) {
-        std::cerr << "TRACE: " << message << " (" << file << ":" << line << ")" << std::endl;
+        std::cerr << "TRACE: " << msg << " (" << file << ":" << line << ")" << std::endl;
     }
 }
 
-inline void logDebug(const std::string& message, const char* file, int line) {
+void logDebug(const std::string &msg, const char *file, int line) {
     if (LOG_LEVEL > 4) {
-        std::cerr << "DEBUG: " << message << " (" << file << ":" << line << ")" << std::endl;
+        std::cerr << "DEBUG: " << msg << " (" << file << ":" << line << ")" << std::endl;
     }
 }
 
-inline void logInfo(const std::string& message, const char* file, int line) {
+void logInfo(const std::string &msg, const char *file, int line) {
     if (LOG_LEVEL > 3) {
-        std::cerr << "INFO: " << message << " (" << file << ":" << line << ")" << std::endl;
+        std::cerr << "INFO: " << msg << " (" << file << ":" << line << ")" << std::endl;
     }
 }
 
-inline void logWarning(const std::string& message, const char* file, int line) {
+void logWarning(const std::string &msg, const char *file, int line) {
     if (LOG_LEVEL > 2) {
-        std::cerr << "WARNING: " << message << " (" << file << ":" << line << ")" << std::endl;
+        std::cerr << "WARNING: " << msg << " (" << file << ":" << line << ")" << std::endl;
     }
 }
 
-inline void logError(const std::string& message, const char* file, int line) {
+void logError(const std::string &msg, const char *file, int line) {
     if (LOG_LEVEL > 1) {
-        std::cerr << "ERROR: " << message << " (" << file << ":" << line << ")" << std::endl;
+        std::cerr << "ERROR: " << msg << " (" << file << ":" << line << ")" << std::endl;
     }
 }
 
-inline void logFatal(const std::string& message, const char* file, int line) {
+void logFatal(const std::string &msg, const char *file, int line) {
     if (LOG_LEVEL > 0) {
-        std::cerr << "FATAL: " << message << " (" << file << ":" << line << ")" << std::endl;
+        std::cerr << "FATAL: " << msg << " (" << file << ":" << line << ")" << std::endl;
     }
 }
 
